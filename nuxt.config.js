a7cdfd131cd6cf2ff094c18f031297fe176ca0dc
Quentin Neyraud <quentin.neyraud@gmail.com>
2020-11-20 00:38:30 +0100

    Setup slice machine

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,47 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'slice-contest',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    meta: [{
+      charset: 'utf-8'
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    }, {
+      hid: 'description',
+      name: 'description',
+      content: ''
+    }],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }],
+    script: []
   },
-
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
-
+  css: [],
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
-
+  plugins: [],
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
-  ],
-
+  buildModules: [// https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module'],
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
-
+  modules: [['@nuxtjs/prismic', {
+    endpoint: '',
+    apiOptions: {
+      routes: [{
+        type: 'page',
+        path: '/:uid'
+      }]
+    }
+  }], ['nuxt-sm']],
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    transpile: ['vue-slicezone', 'nuxt-sm']
   }
 }
